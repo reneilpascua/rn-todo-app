c63b46be499cf8b11e1720ae4040d80d01183a53
reneilpascua <rpascua2@my.bcit.ca>
2020-03-10 01:10:33 -0700

    learned a little about arrow functions

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
+import InputBar from './components/InputBar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      todoInput: '',
+      todos: [
+        {id: 0, title: 'Take out the trash', done: false},
+        {id: 1, title: 'Cook dinner', done: false},
+      ]
+    }
+  }
+
+  render() {
+
+    const statusbar = (Platform.OS == 'ios') ? <View style={styles.statusbar}></View> : <View></View>
+
+    return (
+      <View style={styles.container}>
+        {statusbar}
+        <Header title="To-do App"/>
+        <InputBar textChange={todoInput => this.setState({todoInput})}/>
+
+        <Text>{this.state.todoInput}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  statusbar: {
+    backgroundColor: '#FFCE00',
+    height: 40
+  }
 });
