5a26f1e4f01522ee6ed9c1d07b6738e23fae9ede
reneilpascua <rpascua2@my.bcit.ca>
2020-03-11 02:25:31 -0700

    finished todo app tutorial

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header'
 import InputBar from './components/InputBar';
+import TodoItem from './components/TodoItem'
 
 
 export default class App extends React.Component {
@@ -18,6 +19,43 @@ export default class App extends React.Component {
     }
   }
 
+  addNewTodo() {
+    let todos = this.state.todos;
+
+    todos.unshift({
+      id: todos.length + 1,
+      title: this.state.todoInput,
+      done: false
+    });
+
+    this.setState({
+      todos,
+      todoInput:''
+    })
+  }
+
+  toggleDone(item) {
+    let todos = this.state.todos;
+
+    todos = todos.map((todo) => {
+      if (todo.id == item.id) {
+        todo.done = !todo.done;
+      }
+
+      return todo;
+    });
+
+    this.setState({todos});
+  }
+
+  removeTodo(item) {
+    let todos = this.state.todos;
+
+    todos = todos.filter((todo) => todo.id != item.id);
+
+    this.setState({todos});
+  }
+
   render() {
 
     const statusbar = (Platform.OS == 'ios') ? <View style={styles.statusbar}></View> : <View></View>
@@ -26,7 +64,31 @@ export default class App extends React.Component {
       <View style={styles.container}>
         {statusbar}
         <Header title="To-do App"/>
-        <InputBar textChange={todoInput => this.setState({todoInput})}/>
+        <InputBar 
+          textChange={todoInput => this.setState({todoInput})}
+          addNewTodo={ () => this.addNewTodo()}
+          todoInput={this.state.todoInput}
+        />
+        <FlatList 
+          data={this.state.todos}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem 
+                todoItem={item} 
+                toggleDone={() => {
+                    this.toggleDone(item)
+                  }
+                }
+                removeTodo={() => {
+                    this.removeTodo(item)
+                  }
+                }
+              />
+            )
+          }}
+         />
 
         <Text>{this.state.todoInput}</Text>
       </View>
